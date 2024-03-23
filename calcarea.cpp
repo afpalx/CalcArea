76581d63c83b06da8532be5ae07146bbf1bc1676
warchworch <163400842+afpalx@users.noreply.github.com>
2024-03-23 20:19:29 +0100

    Add files via upload

--- calcarea.cpp
+++ calcarea.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<cmath>
+#include<vector>
 
 class Shape {
 	public:
@@ -37,30 +38,78 @@ class Circle : public Shape {
 		}
 };
 
+void printArea(const Shape& shape) {
+	if(dynamic_cast<const Square*>(&shape)) {
+		std::cout << "Area of square: ";
+	} else if(dynamic_cast<const Rectangle*>(&shape)) {
+		std::cout << "Area of rectangle: ";
+	} else if(dynamic_cast<const Circle*>(&shape)) {
+		std::cout << "Area of circle: ";
+	}
+	std::cout << shape.calcArea() << std::endl;
+}
+
 int main() {
 /**	Square square(3.0);
 	Rectangle rectangle (4.0, 5.0);
 	Circle circle(6.0);
 **/
 	double side, length, width, radius;
+	std::vector<Shape*> shapes;
 
 	std::cout << "Enter side of the square: ";
 	std::cin >> side;
+	shapes.push_back(new Square(side));
+
+/**
 	Square square(side);
+**/
 
 	std::cout << "Enter length of the rectangle: ";
 	std::cin >> length;
 	std::cout << "Enter width of the rectangle: ";
 	std::cin >> width;
+	shapes.push_back(new Rectangle(length, width));
+
+/**
 	Rectangle rectangle(length, width);
+**/
 
 	std::cout << "Enter the radius of the circle: ";
 	std::cin>> radius;
-	Circle circle(radius);
+	shapes.push_back(new Circle(radius));
 
+/**	
+	Circle circle(radius);
+**/
+
+/**
+	for (const auto& shape : shapes) {
+		if (dynamic_cast<Square*>(shape)) {
+			std::cout << "Area of square: ";
+		} else if (dynamic_cast<Rectangle*>(shape)) {
+			std::cout << "Area of rectangle: ";
+		} else if (dynamic_cast<Circle*>(shape)) {
+			std::cout << "Area of circle: ";
+		}
+		std::cout << shape->calcArea() << std::endl;
+	}
+**/
+
+	for(const auto& shape : shapes) {
+		printArea(*shape);
+	}
+
+
+	for (const auto& shape : shapes) {
+		delete shape;
+	}
+
+/**
 	std::cout << "Area of square: " << square.calcArea() << std::endl;
 	std::cout << "Area of rectangle: " << rectangle.calcArea() << std::endl;
 	std::cout << "Area of circle: " << circle.calcArea() << std::endl;
+**/
 
 	return 0;
 }
